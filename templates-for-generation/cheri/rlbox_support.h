d6f9c4a4ed6e4e49b0c2534258aec98a74f4768c
Evan Johnson <e5johnso@eng.ucsd.edu>
2023-02-16 13:25:36 -0800

    add full rlbox support for add_mswasm_callback and remove_mswasm_callback

--- templates-for-generation/cheri/rlbox_support.h
+++ templates-for-generation/cheri/rlbox_support.h
@@ -5,18 +5,19 @@
 
 typedef WasmModule* (*new_wasm_module_t)(i32 argc, Handle argv);
 typedef void (*destroy_wasm_module_t)(WasmModule* ctx);
-typedef void (*remove_callback_t)(WasmModule* ctx, uint32_t callback_idx);
-
+typedef void (*remove_callback_t)(WasmModule* ctx, u32 callback_idx);
+typedef u32 (*add_callback_t)(WasmModule* ctx, u32 func_type_idx, Handle func_ptr);
 
 WasmModule* new_wasm_module(i32 argc, Handle argv);
 void destroy_wasm_module(WasmModule* ctx);
-void remove_mswasm_callback(WasmModule* ctx, uint32_t callback_idx);
+void remove_mswasm_callback(WasmModule* ctx, u32 callback_idx);
+u32 add_mswasm_callback(WasmModule* ctx, u32 func_type_idx, Handle func_ptr);
 
 
 typedef struct mswasm_sandbox_funcs_t {
   new_wasm_module_t create_mswasm_sandbox;
   destroy_wasm_module_t destroy_mswasm_sandbox;
-  // add_callback_t add_mswasm_callback;
+  add_callback_t add_mswasm_callback;
   remove_callback_t remove_mswasm_callback;
 } mswasm_sandbox_funcs_t;
 
@@ -27,11 +28,26 @@ mswasm_sandbox_funcs_t get_mswasm_sandbox_info() {
   ret.destroy_mswasm_sandbox = &destroy_wasm_module; // rwasm naming convention
   // ret.lookup_wasm2c_nonfunc_export = &lookup_wasm2c_nonfunc_export;
   // ret.lookup_wasm2c_func_index = &lookup_wasm2c_func_index;
-  // ret.add_wasm2c_callback = &add_wasm2c_callback;
+  ret.add_mswasm_callback = &add_mswasm_callback;
   ret.remove_mswasm_callback = &remove_mswasm_callback;
   return ret;
 }
 
+
+void remove_mswasm_callback(WasmModule* ctx, uint32_t callback_idx){
+  ctx->indirect_call_table[callback_idx].func_ptr = NULL;
+}
+
+// linearly scan through callback table for a free entry and replace it
+u32 add_mswasm_callback(WasmModule* ctx, u32 func_type_idx, Handle func_ptr){
+  for (int idx = 0; idx < 128; idx++){
+    if (ctx->indirect_call_table[idx].func_ptr == NULL) {
+      ctx->indirect_call_table[idx].type_idx = func_type_idx;
+      ctx->indirect_call_table[idx].func_ptr = func_ptr;
+    }
+  }
+  assert(false && "Exceeded max callback slots (128)");
+}
 
 
 
@@ -40,11 +56,6 @@ mswasm_sandbox_funcs_t get_mswasm_sandbox_info() {
 // //                                                uint32_t param_count,
 // //                                                uint32_t result_count,
 // //                                                wasm_rt_type_t* types);
-// // typedef uint32_t (*add_wasm2c_callback_t)(
-// //     WasmModule* ctx,
-// //     uint32_t func_type_idx,
-// //     void* func_ptr,
-// //     wasm_rt_elem_target_class_t func_class);
 
 
 // typedef struct mswasm_sandbox_funcs_t {
